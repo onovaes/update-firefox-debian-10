e63ff42465d47c1997b74689a19430d1f29c55e3
Gabriel Novaes <semproblema@gmail.com>
2022-10-17 15:36:43 -0400

    Readme and kill firefox only after download has finished

--- update-firefox.sh
+++ update-firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update firewfox on debian 10
 
-pkill firefox
 curl -Lo firefox.tar.bz2 'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US'
+pkill firefox
 sudo tar -xjvf firefox.tar.bz2 -C /opt
 cd /opt/firefox && ./firefox --version
